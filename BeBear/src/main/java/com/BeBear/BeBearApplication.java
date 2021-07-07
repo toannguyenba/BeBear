240551c36bb76f12deb0b38b37cfed6b516daba0
ToanNB <ngbatoan22@gmail.com>
2021-07-07 22:02:43 +0700

    edit config to run

--- BeBear/src/main/java/com/BeBear/BeBearApplication.java
+++ BeBear/src/main/java/com/BeBear/BeBearApplication.java
@@ -2,12 +2,20 @@ package com.BeBear;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 @SpringBootApplication
 public class BeBearApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(BeBearApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	public String index() {
+		return "index";
 	}
 
 }
