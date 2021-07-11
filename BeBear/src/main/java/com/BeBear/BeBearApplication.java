bfe9e94c7a00077c51abc8788a7013ccf3b397dc
tung2310 <ngbatoan22@gmail.com>
2021-07-11 15:32:22 +0700

    commit template admin

--- BeBear/src/main/java/com/BeBear/BeBearApplication.java
+++ BeBear/src/main/java/com/BeBear/BeBearApplication.java
@@ -13,9 +13,9 @@ public class BeBearApplication {
 		SpringApplication.run(BeBearApplication.class, args);
 	}
 	
-	@GetMapping("/")
-	public String index() {
-		return "index";
-	}
+//	@GetMapping("/")
+//	public String index() {
+//		return "index";
+//	}
 
 }
