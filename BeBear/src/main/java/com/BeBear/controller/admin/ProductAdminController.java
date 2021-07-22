2c78be53334ecf86199a0cea1cd42a4408484480
tung2310 <ngbatoan22@gmail.com>
2021-07-22 20:19:05 +0700

    Product list

--- BeBear/src/main/java/com/BeBear/controller/admin/ProductAdminController.java
+++ BeBear/src/main/java/com/BeBear/controller/admin/ProductAdminController.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import com.BeBear.entities.Category;
 import com.BeBear.entities.Product;
+import com.BeBear.repositories.CategoryRepository;
 import com.BeBear.repositories.ProductRepository;
 
 /**
@@ -23,16 +25,21 @@ public class ProductAdminController {
 	@Autowired
 	private ProductRepository productRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
 	@GetMapping("/adminProduct")
 	public String listProduct(Model model) {
 		try {
 			List<Product> products = productRepository.findAll();
+			List<Category> categorys = categoryRepository.findAll();
 //			System.out.println(products.get(0).getIdPhoto());
 			for(Product temp: products ) {
 				temp.setIdPhoto(null);
 				temp.setProductdetail(null);
 			}
 			model.addAttribute("products", products);
+			model.addAttribute("categorys", categorys);
 			model.addAttribute("product", new Product());
 		} catch (Exception e) {
 			System.out.println(e);
---
+++ BeBear/src/main/java/com/BeBear/repositories/CategoryRepository.java
@@ -0,0 +1,11 @@
+package com.BeBear.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.BeBear.entities.Category;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
