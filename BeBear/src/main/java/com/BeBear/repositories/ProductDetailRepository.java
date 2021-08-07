9536b95d85f606127adbbee000210ab0493530ea
batoan2205 <batoan2205@gmail.com>
2021-08-07 14:14:20 +0700

    sua loi

--- BeBear/src/main/java/com/BeBear/repositories/ProductDetailRepository.java
+++ BeBear/src/main/java/com/BeBear/repositories/ProductDetailRepository.java
@@ -8,10 +8,7 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-<<<<<<< HEAD
 import com.BeBear.entities.Color;
-=======
->>>>>>> tungdev
 import com.BeBear.entities.Product;
 import com.BeBear.entities.ProductDetail;
 import com.BeBear.entities.Size;
